4e258750380f4bd8d4509f515f998edd15f08a0f
Denka88 <shadowfightdenka88@gmail.com>
2025-02-27 12:48:04 +0300

    logout done

--- src/main/java/com/example/library/controller/UserController.java
+++ src/main/java/com/example/library/controller/UserController.java
@@ -1,10 +1,11 @@
 package com.example.library.controller;
 
 import com.example.library.dto.UserDto;
-import com.example.library.model.User;
+
 import com.example.library.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,33 +14,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-    
+
     private final UserService userService;
 
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    
+
     @ModelAttribute("user")
-    public UserDto user(){
+    public UserDto user() {
         return new UserDto();
     }
-    
+
     @GetMapping("/registration")
-    String registration(){
+    String registration() {
         return "registration";
     }
-    
+
     @PostMapping("/save")
-    String save(@ModelAttribute("user") UserDto userDto){
+    String save(@ModelAttribute("user") UserDto userDto) {
         userService.save(userDto);
         return "redirect:/login";
     }
 
+    @GetMapping("/")
+    String index(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", userService.findUser(user.getUsername()));
+        return "index";
+    }
+
     @GetMapping("/profile")
     @PreAuthorize("isAuthenticated()")
-    String profile(@AuthenticationPrincipal User user, Model model){
+    String profile(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("user", userService.findUser(user.getUsername()));
         model.addAttribute("title", "Электронная библиотека - профиль пользователя");
         return "profile";
