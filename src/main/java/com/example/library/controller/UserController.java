b2e023562eabe3f6cc7677fc3435d03d4a34954c
Denka88 <shadowfightdenka88@gmail.com>
2025-02-27 14:02:06 +0300

    1402

--- src/main/java/com/example/library/controller/UserController.java
+++ src/main/java/com/example/library/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.library.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,14 +40,14 @@ public class UserController {
     }
 
     @GetMapping("/")
-    String index(@AuthenticationPrincipal User user, Model model) {
+    String index(@AuthenticationPrincipal UserDetails user, Model model) {
         model.addAttribute("user", userService.findUser(user.getUsername()));
         return "index";
     }
 
     @GetMapping("/profile")
     @PreAuthorize("isAuthenticated()")
-    String profile(@AuthenticationPrincipal User user, Model model) {
+    String profile(@AuthenticationPrincipal UserDetails user, Model model) {
         model.addAttribute("user", userService.findUser(user.getUsername()));
         model.addAttribute("title", "Электронная библиотека - профиль пользователя");
         return "profile";
